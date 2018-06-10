829c6ac40ff4eed6f8f50e84ddd2cb95cf65c343
Unknown <takuto.hamano@gmail.com>
2018-06-10 16:35:41 +0900

    Import element-ui

--- webpack/src/router/index.js
+++ webpack/src/router/index.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import 'element-ui/lib/theme-chalk/index.css'
 import Router from 'vue-router'
 import Profile from '@/components/Profile'
 
 Vue.use(Router)
+Vue.use(ElementUI, {locale})
 
 export default new Router({
   routes: [
