a40eeffd970a31a224b4c719a675c31f8576a588
Unknown <takuto.hamano@gmail.com>
2018-06-14 20:42:04 +0900

    Install vue-icon.

--- webpack/src/router/index.js
+++ webpack/src/router/index.js
@@ -3,12 +3,14 @@ import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 import 'element-ui/lib/theme-chalk/index.css'
 import Router from 'vue-router'
+import feather from 'vue-icon'
 import About from '@/components/About'
 import Works from '@/components/Works'
 import Photographs from '@/components/Photographs'
 
 Vue.use(Router)
 Vue.use(ElementUI, {locale})
+Vue.use(feather, 'v-icon')
 
 export default new Router({
   routes: [
