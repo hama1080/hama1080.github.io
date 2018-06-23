e874453871a63f7ec35ac9e7add323620d80166c
Unknown <takuto.hamano@gmail.com>
2018-06-23 14:48:41 +0900

    Remove timerRelease component.

--- webpack/src/router/index.js
+++ webpack/src/router/index.js
@@ -13,7 +13,6 @@ import hiraganaGAN from '@/components/work/hiraganaGAN'
 import bitcoinPriceChecker from '@/components/work/bitcoinPriceChecker'
 import hexMan2 from '@/components/work/hexMan2'
 import hexMan from '@/components/work/hexMan'
-import timerRelease from '@/components/work/timerRelease'
 
 Vue.use(Router)
 Vue.use(ElementUI, {locale})
@@ -60,11 +59,6 @@ export default new Router({
       path: '/work/hexMan',
       name: 'hexMan',
       component: hexMan
-    },
-    {
-      path: '/work/timerRelease',
-      name: 'timerRelease',
-      component: timerRelease
     }
   ]
 })
