261f8acbf72021f199e51f1e9a1daeeb7343bed2
Unknown <takuto.hamano@gmail.com>
2018-06-25 20:56:39 +0900

    Update HiraganaGAN.

--- webpack/src/components/work/hiraganaGAN.vue
+++ webpack/src/components/work/hiraganaGAN.vue
@@ -3,41 +3,38 @@
     <el-row>
       <el-col :span="20" :offset="2">
 
-        <el-card>
+        <el-card class="work-card">
           <el-row>
             <el-col :span="24">
               <h2>ひらがなGAN</h2>
               <h3>2018/3</h3>
 
-              <!-- Description -->
-              <div>
-                GANを用いた手書きひらがな文字の生成
+              <div class="description">
+                <a href="http://dl4us.com/">DL4US</a>の課題で実施した，GANを用いた手書きひらがな文字の生成．<br>
+                学習には，産業技術総合研究所が公開している，ETL文字データベースを利用．<br>
+                学習後に任意のひらがな文字を生成可能とするため，ネットワーク構造にはConditional GANを利用した．<br>
               </div>
 
-              <!-- Screen shots -->
-              <el-carousel trigger="click" indicator-position="outside" :autoplay=false>
-                <el-carousel-item>
-                  <img src="/static/works/hiraganaGAN/learning_flow.gif">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/hiraganaGAN/learning_result.png">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/hiraganaGAN/yakiniku.png">
-                </el-carousel-item>
-              </el-carousel>
+              <div class="screen-shots">
+                <el-carousel trigger="click" indicator-position="outside" :autoplay=false>
+                  <el-carousel-item>
+                    <img src="/static/works/hiraganaGAN/learning_flow.gif">
+                  </el-carousel-item>
+                  <el-carousel-item>
+                    <img src="/static/works/hiraganaGAN/learning_result.png">
+                  </el-carousel-item>
+                  <el-carousel-item>
+                    <img src="/static/works/hiraganaGAN/yakiniku.png">
+                  </el-carousel-item>
+                </el-carousel>
+              </div>
 
-              <!-- Technologies -->
-              <div>
+              <div class="technologies">
                 <span class="tech">Technologies:</span>
                 <span class="tech">Python</span>
                 <span class="tech">Keras(TensorFlow)</span>
-                <span class="tech">GAN(Generative Adversarial Network)</span>
+                <span class="tech">Conditional GAN</span>
               </div>
-
-              <!-- Source link -->
-              <sourceLink address=""></sourceLink>
-
             </el-col>
           </el-row>
 
@@ -60,6 +57,13 @@ export default {
 </script>
 
 <style scoped>
+.work-card{
+  margin: 0 0 50px 0;
+}
+
+.description, .screen-shots, .description{
+  margin: 30px 0;
+}
 
 .tech{
   margin: 0 5px;
