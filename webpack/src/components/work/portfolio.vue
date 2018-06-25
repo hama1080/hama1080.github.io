043b50e342bedf0f20dcb3425b997da2a2be1a66
Unknown <takuto.hamano@gmail.com>
2018-06-25 20:47:29 +0900

    Update portfolio.

--- webpack/src/components/work/portfolio.vue
+++ webpack/src/components/work/portfolio.vue
@@ -3,32 +3,29 @@
     <el-row>
       <el-col :span="20" :offset="2">
 
-        <el-card>
+        <el-card class="work-card">
           <el-row>
             <el-col :span="24">
               <h2>Portfolio</h2>
               <h3>2018/6</h3>
 
-              <!-- Description -->
-              <div>
-                Vue.jsを利用したポートフォリオサイト
+              <div class="description">
+                Vue.jsを利用したポートフォリオサイト．<br>
+                UIライブラリとしてElement UIを利用．<br>
+                これまでに作ったものや，撮影した写真を掲載．<br>
               </div>
 
-              <!-- Screen shots -->
-              <el-carousel trigger="click" indicator-position="outside" :autoplay=false>
-                <el-carousel-item>
-                  <img src="/static/works/portfolio/screenshot.png">
-                </el-carousel-item>
-              </el-carousel>
+              <div class="screen-shots">
+                <img src="/static/works/portfolio/figure0.png">
+              </div>
 
-              <!-- Technologies -->
-              <div>
+              <div class="technologies">
                 <span class="tech">Technologies:</span>
                 <span class="tech">HTML/CSS</span>
                 <span class="tech">Vue.js</span>
+                <span class="tech">Element UI</span>
               </div>
 
-              <!-- Source link -->
               <sourceLink address="https://github.com/hama903/hama903.github.io"></sourceLink>
 
             </el-col>
@@ -53,6 +50,13 @@ export default {
 </script>
 
 <style scoped>
+.work-card{
+  margin: 0 0 50px 0;
+}
+
+.description, .screen-shots, .description{
+  margin: 30px 0;
+}
 
 .tech{
   margin: 0 5px;
