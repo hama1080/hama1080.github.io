7d0071f8c3da9045c08a2f2d9ed803054c649468
Unknown <takuto.hamano@gmail.com>
2018-06-29 22:08:14 +0900

    Use work component in portfolio.

--- webpack/src/components/work/portfolio.vue
+++ webpack/src/components/work/portfolio.vue
@@ -1,70 +1,27 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="20" :offset="2">
-
-        <el-card class="work-card">
-          <el-row>
-            <el-col :span="24">
-              <h2>Portfolio</h2>
-              <h3>2018/6</h3>
-
-              <div class="description">
-                <el-row>
-                  <el-col :md="{span: 16, offset: 4}">
-                    Vue.jsを利用したポートフォリオサイト．UIライブラリとしてElement UIを利用．これまでに作ったものや，撮影した写真を掲載．
-                  </el-col>
-                </el-row>
-              </div>
-
-              <div class="screen-shots">
-                <img src="/static/works/portfolio/figure0.png" style="max-width: 100%">
-              </div>
-
-              <div class="technologies">
-                <span class="tech">Technologies:</span>
-                <span class="tech">HTML/CSS</span>
-                <span class="tech">Vue.js</span>
-                <span class="tech">Element UI</span>
-              </div>
-
-              <sourceLink address="https://github.com/hama903/hama903.github.io"></sourceLink>
-
-            </el-col>
-          </el-row>
-
-        </el-card>
-
-      </el-col>
-    </el-row>
+    <work
+      title="Portfolio"
+      date="2018"
+      description="Vue.jsを利用したポートフォリオサイト．UIライブラリとしてElement UIを利用．これまでに作ったものや，撮影した写真を掲載．"
+      v-bind:screenShots="['/static/works/portfolio/figure0.png']"
+      v-bind:technologies="['HTML/CSS', 'Vue.js', 'Element UI']"
+      sourceAddress="https://github.com/hama903/hama903.github.io"
+    ></work>
   </div>
 </template>
 
 <script>
-import sourceLink from './sourceLink.vue'
+import work from './work.vue'
 
 export default {
   name: 'portfolio',
   components: {
-    sourceLink
+    work
   }
 }
 </script>
 
 <style scoped>
-.work-card{
-  margin: 0 0 50px 0;
-}
-
-.description{
-  text-align: left;
-}
-.description, .screen-shots, .description{
-  margin: 30px 0;
-}
-
-.tech{
-  margin: 0 5px;
-}
 
 </style>
