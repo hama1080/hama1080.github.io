655557746310f94a31cae029b3b779167a7ee999
Unknown <takuto.hamano@gmail.com>
2018-06-30 19:00:23 +0900

    Use work component in fileViewer.

--- webpack/src/components/work/fileViewer.vue
+++ webpack/src/components/work/fileViewer.vue
@@ -1,69 +1,27 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="20" :offset="2">
-
-        <el-card>
-          <el-row>
-            <el-col :span="24">
-              <h2>File Viewer Art</h2>
-              <h3>2012</h3>
-
-              <!-- Description -->
-              <div>
-                ファイルの階層構造を，エクスプローラーとは違った形式で表示したメディアアート作品．
-                あるディレクトリに含まれるディレクトリは中央付近で回転し，ファイルは周囲を飛び回る．
-                操作感は非常に悪いが，見た目的には楽しいものとなっている．
-              </div>
-
-              <!-- Screen shots -->
-              <el-carousel trigger="click" indicator-position="outside" :autoplay=false>
-                <el-carousel-item>
-                  <img src="/static/works/fileViewer/animation.gif">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/fileViewer/figure0.png">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/fileViewer/figure1.png">
-                </el-carousel-item>
-              </el-carousel>
-
-              <!-- Technologies -->
-              <div>
-                <span class="tech">Technologies:</span>
-                <span class="tech">C++</span>
-                <span class="tech">MFC</span>
-              </div>
-
-              <!-- Source link -->
-              <!-- <sourceLink address=""></sourceLink> -->
-
-            </el-col>
-          </el-row>
-
-        </el-card>
-
-      </el-col>
-    </el-row>
+    <work
+      title="File Viewer Art"
+      date="2012"
+      description="ファイルの階層構造を，エクスプローラーとは違った形式で表示したメディアアート作品．あるディレクトリに含まれるディレクトリは中央付近で回転し，ファイルは周囲を飛び回る．操作感は非常に悪いが，見た目的には楽しいものとなっている．"
+      v-bind:screenShots="['/static/works/fileViewer/animation.gif', '/static/works/fileViewer/figure0.png', '/static/works/fileViewer/figure1.png']"
+      v-bind:technologies="['C++', 'MFC']"
+      sourceAddress=""
+    ></work>
   </div>
 </template>
 
 <script>
-import sourceLink from './sourceLink.vue'
+import work from './work.vue'
 
 export default {
-  name: 'portfolio',
+  name: 'hiraganaGAN',
   components: {
-    sourceLink
+    work
   }
 }
 </script>
 
 <style scoped>
-
-.tech{
-  margin: 0 5px;
-}
 
 </style>
