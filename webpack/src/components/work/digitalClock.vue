7ed3d7e051ea7ce40f8dacf6de416fd61c385063
Unknown <takuto.hamano@gmail.com>
2018-06-30 18:56:05 +0900

    Use work component in digitalClock.

--- webpack/src/components/work/digitalClock.vue
+++ webpack/src/components/work/digitalClock.vue
@@ -1,99 +1,27 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="20" :offset="2">
-
-        <el-card>
-          <el-row>
-            <el-col :span="24">
-              <h2>DigitalClock</h2>
-              <h3>2013</h3>
-
-              <!-- Description -->
-              <div>
-                Arduinoと7セグメントLEDを用いたデジタル時計．
-                4桁の7セグLEDをダイナミック点灯方式を利用し，点灯させている．
-                制御基板と表示基板を分けて作成し，立体的に連結することで，スタイリッシュなデザインとしている．
-              </div>
-
-              <!-- Screen shots -->
-              <el-carousel trigger="click" indicator-position="outside" :autoplay=false>
-                <el-carousel-item>
-                  <img src="/static/works/digitalClock/figure0.png">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/digitalClock/figure1.png">
-                </el-carousel-item>
-                <el-carousel-item>
-                  <img src="/static/works/digitalClock/figure2.png">
-                </el-carousel-item>
-              </el-carousel>
-
-              <!-- Technologies -->
-              <div>
-                <span class="tech">Technologies:</span>
-                <span class="tech">Arduino</span>
-                <span class="tech">Dynamic Lighting System</span>
-              </div>
-
-              <div>
-                <div>主な利用部品</div>
-                <el-table :data="tableData" style="width: 80%; margin: 0 auto;">
-                  <el-table-column align="center" prop="part_name" label="材料名"></el-table-column>
-                  <el-table-column align="center" prop="model_number" label="型番"></el-table-column>
-                  <el-table-column align="center" prop="usage" label="用途"></el-table-column>
-                </el-table>
-              </div>
-
-              <!-- Source link -->
-              <sourceLink address="https://github.com/hama903/ArduinoDigitalClock"></sourceLink>
-
-            </el-col>
-          </el-row>
-
-        </el-card>
-
-      </el-col>
-    </el-row>
+    <work
+      title="DigitalClock"
+      date="2013"
+      description="Arduinoと7セグメントLEDを用いたデジタル時計．4桁の7セグLEDをダイナミック点灯方式を利用し，点灯させている．制御基板と表示基板を分けて作成し，立体的に連結することで，スタイリッシュなデザインとしている．"
+      v-bind:screenShots="['/static/works/digitalClock/figure0.png', '/static/works/digitalClock/figure1.png', '/static/works/digitalClock/figure2.png']"
+      v-bind:technologies="['Arduino', 'Dynamic Lighting System']"
+      sourceAddress="https://github.com/hama903/ArduinoDigitalClock"
+    ></work>
   </div>
 </template>
 
 <script>
-import sourceLink from './sourceLink.vue'
+import work from './work.vue'
 
 export default {
-  name: 'digitalClock',
+  name: 'hiraganaGAN',
   components: {
-    sourceLink
-  },
-  data () {
-    return {
-      tableData: [
-        {
-          part_name: 'マイコン',
-          model_number: 'Arduino Uno(ATMega328P)',
-          usage: '時計制御用'
-        },
-        {
-          part_name: '7セグメントLED × 4',
-          model_number: 'C-1001UB',
-          usage: '時計表示用'
-        },
-        {
-          part_name: 'トランジスタアレイ',
-          model_number: 'TD62003AP, M54563P',
-          usage: '電流増幅用'
-        }
-      ]
-    }
+    work
   }
 }
 </script>
 
 <style scoped>
-
-.tech{
-  margin: 0 5px;
-}
 
 </style>
